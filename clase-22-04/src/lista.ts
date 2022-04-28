46cb6396036d8bc63a2b9db79e8aec0963d2ad2d
RomiDominguez <104107320+RomiDominguez@users.noreply.github.com>
2022-04-28 17:14:59 -0300

    Initial commit

---
+++ clase-22-04/src/lista.ts
@@ -0,0 +1,36 @@
+console.log("RECETA BROWNIE DE CHOCOLATE");
+console.log(
+  "Necesitaremos: \n · Un bowl grande \n · Cuchara de madera\n · Una taza \n · Una sartén"
+);
+
+console.log("LISTA DE INGREDIENTES:");
+console.log("· 100gr de manteca");
+console.log("· 150gr de chocolate");
+console.log("· 2 huevos");
+console.log("· 1 taza de azúcar");
+console.log("· 1/2 taza de nueces picadas");
+console.log("· 100gr de harina \n");
+
+console.log("PREPARACIÓN:");
+console.log(
+  "1° Colocar la manteca y el chocolate picados en una sartén, y llevarlos a fuego bien bajo. Lo tapamos y vamos a dejarlo por unos 5 minutos sin tocar."
+);
+console.log(
+  "2° Retirar del fuego y revolver los ingredientes hasta que esté todo derretido e integrado."
+);
+console.log(
+  "3° Batir los 2 huevos con el azúcar hasta que queden bien blancos, esto es clave para que el brownie casero quede bien húmedo."
+);
+console.log(
+  "4° Agregar el chocolate derretido y batir hasta que esté integrado. Sumar las nueces en pedazos grandes o como más les guste."
+);
+console.log(
+  "5° Sumar el harina 0000 tamizada en dos partes e integrar todo como se ve en el video. Es importante batir fuerte antes de pasar el brownie de chocolate al molde."
+);
+console.log(
+  "6° Colocar en una placa y cocinar al horno fuerte (200-220°) por 20 minutos. \n"
+);
+
+console.log(
+  "Recomendación: es mejor sacarlo antes y si lo queremos más cocido volver a colocarlo en el horno unos minutos más. Lo importante es no pasarse para que el brownie no quede seco."
+);
