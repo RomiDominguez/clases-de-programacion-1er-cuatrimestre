be0392baa19070b5a4c6ed86aa3ceb12b4f3df93
RomiDominguez <104107320+RomiDominguez@users.noreply.github.com>
2022-05-12 19:50:47 -0300

    2 ejercicios nuevos Se agregaron 2 ejercicios

---
+++ clase-22-04/6-pasos-de-preparacion/datos.ts
@@ -0,0 +1,18 @@
+let recetaTitulo: string = "RECETA DE BROWNIE \n";
+
+let listaDeIngredientes: string = "Lista de Ingredientes:";
+let ingrediente1: string = "· 100gr de manteca";
+let ingrediente2: string = "· 150gr de chocolate";
+let ingrediente3: string = "· 2 huevos";
+let ingrediente4: string = "· 1 taza de azúcar";
+let ingrediente5: string = "· 1/2 taza de nueces picadas";
+let ingrediente6: string = "· 100gr de harina \n";
+
+console.log(recetaTitulo);
+console.log(listaDeIngredientes);
+console.log(ingrediente1);
+console.log(ingrediente2);
+console.log(ingrediente3);
+console.log(ingrediente4);
+console.log(ingrediente5);
+console.log(ingrediente6);
